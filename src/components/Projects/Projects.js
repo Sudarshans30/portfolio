1328d4231afb58fb23fe5311b319e6fcc8503001
Sudarshan singh <sudarshans471@gmail.com>
2023-05-12 14:21:08 +1000

    added logo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,7 @@ import Platepals from "../../Assets/Projects/Platepals.png";
 import TalentBridge from "../../Assets/Projects/TalentBridge.jpg";
 import WeatherDashboard from "../../Assets/Projects/WeatherDashboard.png";
 import WorkdaySchedular from "../../Assets/Projects/WorkdaySchedular.png";
-
+import Bazaarify from '../../Assets/Projects/Bazaarify.png';
 
 function Projects() {
   return (
@@ -25,6 +25,22 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Bazaarify}
+              isBlog={false}
+              title="Bazaarify"
+              description="This was an multi-vendor ecommerce paltform made with using MERN technologies, the application lets the user to manage and sell their products. To access the admin account, login with username and password: admin@a.com & Pass123456"
+              ghLink="https://github.com/Sudarshans30/Bazarify"
+              demoLink="https://bzfiyapp.herokuapp.com/"
+              
+            />
+          </Col>
+
+
+
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={TalentBridge}
@@ -49,12 +65,47 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={WeatherDashboard}
+              isBlog={false}
+              title=" Weather Dashboard"
+              description=" This application uses an openweather API to display weather data to the user based on which city they search for. this application also displays the weather data for the next five days of the searched city"
+              ghLink = " https://github.com/Sudarshans30/weather-Dashboard"
+              demoLink="https://sudarshans30.github.io/weather-Dashboard/ "    
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={WorkdaySchedular}
+              isBlog={false}
+              title="Workday schedular"
+              description="This application is used as a peronal planner. Its a simple calender app for scheduling the user's work day. The user can edit, save and delete notes"
+              ghLink=" https://github.com/Sudarshans30/work-Day-Schedular"
+              demoLink=" https://sudarshans30.github.io/work-Day-Schedular/"    
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={ NoteTakingAPP }
               isBlog={false}
               title="NoteTaking app"
               description=" This application utilise Express.js, to allow users to create notes that will be saved on the server, and to edit and delete them afterwards as well."
               ghLink=" https://github.com/Sudarshans30/Note-Taker"
               demoLink="https://deploying-note-taking-web.herokuapp.com/"
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={PasswordGenerator}
+              isBlog={false}
+              title="Password generator"
+              description="  This application mostly relies on javascript to executes a number of different function to generate a password. When the user will click the password button and answer the prompts, the javascript will executes and generate the random password as per the prompts."
+              ghLink="https://github.com/Sudarshans30/password-generator-in-javascript"
+              demoLink=" https://sudarshans30.github.io/password-generator-in-javascript/" 
             />
           </Col>
 
@@ -83,41 +134,12 @@ function Projects() {
 
           
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={PasswordGenerator}
-              isBlog={false}
-              title="Password generator"
-              description="  This application mostly relies on javascript to executes a number of different function to generate a password. When the user will click the password button and answer the prompts, the javascript will executes and generate the random password as per the prompts."
-              ghLink="https://github.com/Sudarshans30/password-generator-in-javascript"
-              demoLink=" https://sudarshans30.github.io/password-generator-in-javascript/" 
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={WorkdaySchedular}
-              isBlog={false}
-              title="Workday schedular"
-              description="This application is used as a peronal planner. Its a simple calender app for scheduling the user's work day. The user can edit, save and delete notes"
-              ghLink=" https://github.com/Sudarshans30/work-Day-Schedular"
-              demoLink=" https://sudarshans30.github.io/work-Day-Schedular/"    
-            />
-          </Col>
+          
+          
 
           
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={WeatherDashboard}
-              isBlog={false}
-              title=" Weather Dashboard"
-              description=" This application uses an openweather API to display weather data to the user based on which city they search for. this application also displays the weather data for the next five days of the searched city"
-              ghLink = " https://github.com/Sudarshans30/weather-Dashboard"
-              demoLink="https://sudarshans30.github.io/weather-Dashboard/ "    
-            />
-          </Col>
-
+        
 
      
           
